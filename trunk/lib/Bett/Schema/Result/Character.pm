744f36dbcadf2ac92fa62ad8a109255c5710fa8e
drbean <drbean@b95c5cb7-1b23-de11-8004-000bcdefe56b>
2011-08-12 04:42:38 +0000

    With Characters haskell program, insert character names for exercise in db, use in form select options.

--- trunk/lib/Bett/Schema/Result/Character.pm
+++ trunk/lib/Bett/Schema/Result/Character.pm
@@ -23,7 +23,7 @@ __PACKAGE__->table("character");
 
 =head1 ACCESSORS
 
-=head2 identity
+=head2 entity
 
   data_type: 'text'
   is_nullable: 0
@@ -33,19 +33,26 @@ __PACKAGE__->table("character");
   data_type: 'text'
   is_nullable: 1
 
+=head2 exercise
+
+  data_type: 'text'
+  is_nullable: 0
+
 =cut
 
 __PACKAGE__->add_columns(
-  "identity",
+  "entity",
   { data_type => "text", is_nullable => 0 },
   "string",
   { data_type => "text", is_nullable => 1 },
+  "exercise",
+  { data_type => "text", is_nullable => 0 },
 );
-__PACKAGE__->set_primary_key("identity");
+__PACKAGE__->set_primary_key("entity", "exercise");
 
 
-# Created by DBIx::Class::Schema::Loader v0.07010 @ 2011-08-11 07:21:25
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:nyu2Ca3Zoob4OLDH6TVu6w
+# Created by DBIx::Class::Schema::Loader v0.07010 @ 2011-08-11 15:08:30
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:+dpPHeiAfKfyfR+E9/4X5Q
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
