b2c8056dd019ffab6ed88d6d4b7477a78e62010e
drbean <drbean@b95c5cb7-1b23-de11-8004-000bcdefe56b>
2011-08-12 04:55:44 +0000

    chaining

--- trunk/lib/Bett/Controller/Game.pm
+++ trunk/lib/Bett/Controller/Game.pm
@@ -27,6 +27,35 @@ sub index :Path :Args(0) {
     $c->stash( template => 'exchange.tt2' );
 }
 
+=cut
+
+=head2 base
+
+Can place common logic to start chained dispatch here
+
+=cut
+
+sub base :Chained('/') :PathPart('game') :CaptureArgs(1) {
+	my ($self, $c, $exercise) = @_;
+	$c->stash(game => $c->model('DB::Game'));
+	$c->stash(play => $c->model('DB::Play'));
+	$c->stash(exercise => $exercise);
+	my @chars = $c->model('DB::Character')->
+		search({ exercise => $exercise})->
+		get_column("string")->all;
+	$c->stash( characters => \@chars );
+}
+
+=head2 exchange
+
+=cut
+
+sub url_create :Chained('base') :PathPart('') :Args(0) {
+
+    my ( $self, $c ) = @_;
+    $c->stash( config => $c->config );
+    $c->stash( template => 'exchange.tt2' );
+}
 
 =head1 AUTHOR
 
