d84ba6b9050c6a7eb2630b0f6a0d90d58bea9352
drbean <drbean@b95c5cb7-1b23-de11-8004-000bcdefe56b>
2011-08-16 07:27:50 +0000

    No index, base, chars. Instead, setup, wordschars, exchange actions.

--- trunk/lib/Bett/Controller/Game.pm
+++ trunk/lib/Bett/Controller/Game.pm
@@ -17,55 +17,79 @@ Catalyst Controller.
 =cut
 
 
-=head2 index
+#=head2 index
+#
+#=cut
+#
+#sub index :Path :Args(0) {
+#    my ( $self, $c ) = @_;
+#    $c->stash( config => $c->config );
+#    $c->stash( template => 'exchange.tt2' );
+#}
 
 =cut
 
-sub index :Path :Args(0) {
-    my ( $self, $c ) = @_;
-    $c->stash( config => $c->config );
-    $c->stash( template => 'exchange.tt2' );
+=head2 setup
+
+Initialize games for player in exercise.
+
+=cut
+
+sub setup :Chained('/') :PathPart('game') :CaptureArgs(0) {
+	my ($self, $c) = @_;
+	my $player = $c->session->{player_id};
+	my $exercise = $c->session->{exercise};
+	my $league = $c->session->{league};
+	my %standing;
+	for my $course ( "WH", "YN", "S" ) {
+		my $chances = $c->config->{$course}->{chances};
+		my $cOURSE = ucfirst ( lc $course );
+		my $standing = $c->model("DB::$cOURSE")
+			->find_or_create({ player => $player,
+			exercise => $exercise,
+			league => $league,
+			try => 0,
+			score => 0,
+			questionchance => $chances->{question},
+			answerchance => $chances->{answer}
+			});
+		my %standing = (
+			tries => $standing->try,
+			score => $standing->score,
+			questions => $standing->questionchance,
+			answers => $standing->answerchance);
+		$c->stash( $course => \%standing );
+	}
+	$c->stash(exercise => $exercise);
+	$c->stash(player => $player);
+	$c->stash(league => $league);
 }
 
-=cut
-
-=head2 base
-
-Can place common logic to start chained dispatch here
+=head2 wordschars
 
 =cut
 
-sub base :Chained('/') :PathPart('game') :CaptureArgs(1) {
-	my ($self, $c, $exercise) = @_;
-	$c->stash(game => $c->model('DB::Game'));
-	$c->stash(play => $c->model('DB::Play'));
-	$c->stash(exercise => $exercise);
+sub wordschars :Chained('setup') :PathPart('') :CaptureArgs(0) {
+	my ( $self, $c ) = @_;
+	my $exercise = $c->stash->{exercise};
 	my @chars = $c->model('DB::Character')->
 		search({ exercise => $exercise})->
 		get_column("string")->all;
 	$c->stash( characters => \@chars );
+	$c->stash(words => 
+		$c->model('DB::Word')->find(
+		{exercise => $exercise})->string );
+	$c->stash->{exercise => $exercise};
 }
 
 =head2 exchange
 
 =cut
 
-sub exchange :Chained('words') :PathPart('') :Args(0) {
+sub exchange :Chained('wordschars') :PathPart('') :Args(0) {
     my ( $self, $c ) = @_;
     $c->stash( config => $c->config );
     $c->stash( template => 'exchange.tt2' );
-}
-
-=head2 words
-
-=cut
-
-sub words :Chained('base') :PathPart('') :CaptureArgs(0) {
-	my ( $self, $c ) = @_;
-	my $exercise = $c->stash->{exercise};
-	$c->stash(words => 
-		$c->model('DB::Word')->find(
-		{exercise => $exercise})->string );
 }
 
 =head1 AUTHOR
