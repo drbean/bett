b8042b8989ffa400787c3fbe2bd439175851c8ca
drbean <drbean@b95c5cb7-1b23-de11-8004-000bcdefe56b>
2011-10-09 07:51:24 +0000

    MooseX::Getopt for area, story

--- script/words.pl
+++ script/words.pl
@@ -5,24 +5,39 @@ use warnings;
 use lib 'lib';
 
 use Config::General;
-use Cwd;
-use File::Spec;
-use Grades;
 use YAML qw/LoadFile/;
-
 use Bett::Model::DB;
 use Bett::Schema;
+
+
+package Script;
+
+use Moose;
+with 'MooseX::Getopt';
+
+has 'man'  => ( is => 'ro', isa => 'Bool' );
+has 'help' => ( is => 'ro', isa => 'Bool' );
+has 'area' => (
+    traits => ['Getopt'], is => 'ro', isa => 'Str', required => 1,
+    cmd_aliases => 'a',);
+has 'story' => (
+    traits => ['Getopt'], is => 'ro', isa => 'Str', required => 0,
+    cmd_aliases => 's',);
+
+package main;
 
 my %config = Config::General->new( "bett.conf" )->getall;
 my $connect_info = Bett::Model::DB->config->{connect_info};
 my $schema = Bett::Schema->connect( $connect_info );
 
-my $ex = Grades::Script->new_with_options->exercise;
-my $words = qx"/home/drbean/class/conversation/marriage/$ex/Words";
+my $script = Script->new_with_options;
+my $area = $script->area;
+my $story = $script->story;
+my $words = qx"/home/drbean/class/conversation/$area/$story/Words";
 chomp $words;
 
 my @words = ( [ qw/exercise string/ ] );
-push @words, [ $ex, $words ];
+push @words, [ $story, $words ];
 uptodatepopulate( 'Word', \@words );
 
 sub uptodatepopulate
@@ -45,7 +60,7 @@ script/words.pl - populate word table from Parsing.hs
 
 =head1 SYNOPSIS
 
-perl script/words.pl -x clay
+perl script/words.pl -a marriage -s clay
 
 =head1 DESCRIPTION
 
