6517e5444004014dc2bedb52c5457e021905c5a7
drbean <drbean@b95c5cb7-1b23-de11-8004-000bcdefe56b>
2011-12-18 10:19:19 +0000

    Model characters, not Story_Cats proper_names, object_names

--- trunk/script/Characters.hs
+++ trunk/script/Characters.hs
@@ -4,7 +4,5 @@ import Cats
 import Story_Cats
 
 main = do
-	putStr $ concat $ map (\x ->
-		unlines [x, show $ maybe Unspec id $ lookup x characters]) $
-			map (phon . head) $ proper_names ++ object_names
+	putStr $ concat $ map (\x -> unlines [fst x, show $ snd x] ) characters
 
