958491f76d810f1b73327fe2a6abd43de6f3e53a
drbean <drbean@b95c5cb7-1b23-de11-8004-000bcdefe56b>
2012-07-28 09:29:05 +0000

    config

--- lib/Bett/View/Email.pm
+++ lib/Bett/View/Email.pm
@@ -4,15 +4,7 @@ use strict;
 use base 'Catalyst::View::Email';
 
 __PACKAGE__->config(
-    stash_key => 'email',
-    sender => {
-    mailer => 'SMTP',
-    mailer_args => {
-        Host     => 'smtp.example.com',
-        username => 'username',
-        password => 'password',
-        }
-    }
+    stash_key => 'email'
 );
 
 =head1 NAME
