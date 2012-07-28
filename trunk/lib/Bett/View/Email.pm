cc947e888e33d53670a411f63ae0f1338fffea93
drbean <drbean@b95c5cb7-1b23-de11-8004-000bcdefe56b>
2012-07-28 07:29:04 +0000

    0.31

---
+++ trunk/lib/Bett/View/Email.pm
@@ -0,0 +1,41 @@
+package Bett::View::Email;
+
+use strict;
+use base 'Catalyst::View::Email';
+
+__PACKAGE__->config(
+    stash_key => 'email'
+    sender => {
+    mailer => 'SMTP',
+    mailer_args => {
+        Host     => 'smtp.example.com',
+        username => 'username',
+        password => 'password',
+        }
+    }
+);
+
+=head1 NAME
+
+Bett::View::Email - Email View for Bett
+
+=head1 DESCRIPTION
+
+View for sending email from Bett. 
+
+=head1 AUTHOR
+
+Dr Bean
+
+=head1 SEE ALSO
+
+L<Bett>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
