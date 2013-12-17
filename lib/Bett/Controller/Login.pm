d703372b78e2db6dda9332dcecdc8107a0d639c4
drbean <drbean@b95c5cb7-1b23-de11-8004-000bcdefe56b>
2013-12-17 13:08:10 +0000

    dic:r1484.get_genre_from_exercise:belongs_to.get_genre_from_league:has_one

--- lib/Bett/Controller/Login.pm
+++ lib/Bett/Controller/Login.pm
@@ -51,7 +51,15 @@ sub index :Path :Args(0) {
 			my @memberships = $c->model("dicDB::Member")->search
 				({player => $id});
 			my @leagues;
-			push @leagues, $_->league for @memberships;
+			my $exercise = $c->session->{exercise} || $c->request->query_params
+					->{exercise};
+			my $genre = $c->model("DB::Exercise")->search( {id => $exercise })
+				->first->genre;
+			$c->session->{genre} = $genre;
+			for my $membership (@memberships) {
+				push @leagues, $membership->league if
+					$membership->league->genre->genre eq $genre;
+			}
 			if ( @leagues > 1 ) {
 				$c->stash->{id} = $id;
 				$c->stash->{username} = $name;
