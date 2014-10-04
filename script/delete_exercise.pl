f5ddcfa0a4f519de21a7b66995333483a62ec1c0
drbean <drbean@b95c5cb7-1b23-de11-8004-000bcdefe56b>
2014-10-04 04:01:45 +0000

    Config::General^bett.yaml

--- script/delete_exercise.pl
+++ script/delete_exercise.pl
@@ -27,7 +27,7 @@ has 'genre' => (
 
 package main;
 
-my %config = Config::General->new( "bett.conf" )->getall;
+my $config = LoadFile "bett.yaml";
 my $connect_info = Bett::Model::DB->config->{connect_info};
 my $schema = Bett::Schema->connect( $connect_info );
 
