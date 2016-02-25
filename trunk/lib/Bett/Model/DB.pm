f5698c3263400e2dce56006d5f56ed73b7c36d04
drbean <drbean@b95c5cb7-1b23-de11-8004-000bcdefe56b>
2016-02-25 01:55:47 +0000

    032^042

--- trunk/lib/Bett/Model/DB.pm
+++ trunk/lib/Bett/Model/DB.pm
@@ -6,7 +6,7 @@ use base 'Catalyst::Model::DBIC::Schema';
 # use Bett;
 
 # my $name = Bett->config->{database};
-my $name = "bett032";
+my $name = "bett042";
 
 __PACKAGE__->config(
     schema_class => 'Bett::Schema',
