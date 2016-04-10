269e4ed15ebbf8715d7212ae7baf0b258e620ed6
drbean <drbean@b95c5cb7-1b23-de11-8004-000bcdefe56b>
2016-04-10 06:48:52 +0000

    on_haskell_Transfer_error_message,comment_out_confusing_broken_answer_part

--- trunk/lib/Bett/Controller/Play.pm
+++ trunk/lib/Bett/Controller/Play.pm
@@ -178,10 +178,6 @@ sub evaluate :Chained('try') :PathPart('') :CaptureArgs(0) {
     #    $c->stash->{error_msg} = "The question, '$question' was a correct question, but Bett doesn't know the answer. Report the problem to Dr Bean.";
     #    $c->stash->{unhandled} = $theanswer;
     #}
-	elsif ( $theanswer =~ m/Transfer_\w+: .*$/ ) {
-        $c->stash->{error_msg} = "The question, '$question' was a correct question, and Bett understands there is an answer, but it is broken and it can't work out the answer. Report the problem to Dr Bean.";
-        $c->stash->{unhandled} = $theanswer;
-    }
 	elsif ( $course and $expectedcourse and ($expectedcourse ne 'Unparseable') and ($course ne $expectedcourse ) ) {
 			$c->stash->{error_msg} =
 "'$question' is not a $translate{$course}. It's a $translate{$expectedcourse}.";
@@ -202,6 +198,10 @@ sub evaluate :Chained('try') :PathPart('') :CaptureArgs(0) {
 	elsif ( $myanswer ) {
 		  $c->stash->{status_msg} = "The question, '$question' was a grammatical question."
 	}
+	elsif ( $theanswer =~ m/Transfer_\w+: .*$/ ) {
+        $c->stash->{error_msg} = "The question, '$question' was a correct question."; #, and Bett understands there is an answer, but it is broken and it can't work out the answer. Report the problem to Dr Bean.";
+        $c->stash->{unhandled} = $theanswer;
+    }
 	#elsif ( @thewhanswers and any { $_ eq $myanswer } @thewhanswers ) {
     #        $c->stash->{status_msg} = "'$myanswer' is a correct answer to '$question'. " .
     #"The full list of correct answers is: '$thewhanswers'.";
