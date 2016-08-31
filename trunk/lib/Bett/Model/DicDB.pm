a1fbd79228b2b474be38229b5a13c7bee94913fc
drbean <drbean@b95c5cb7-1b23-de11-8004-000bcdefe56b>
2016-08-31 02:52:43 +0000

    s/dic,bett042/bett051/

--- trunk/lib/Bett/Model/DicDB.pm
+++ trunk/lib/Bett/Model/DicDB.pm
@@ -7,7 +7,7 @@ use base 'Catalyst::Model::DBIC::Schema';
 # use Bett;
 
 # my $name = Bett->config->{dic_database};
-my $name = "dic042";
+my $name = "dic051";
 
 __PACKAGE__->config(
     schema_class => 'Bett::DicSchema',
