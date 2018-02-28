5dcf88959b42379e2d236dc30745a58fa214177d
drbean <drbean@freeshell.org>
2018-02-28 16:40:58 +0900

    s/061/062/

--- lib/Bett/Model/DB.pm
+++ lib/Bett/Model/DB.pm
@@ -6,7 +6,7 @@ use base 'Catalyst::Model::DBIC::Schema';
 # use Bett;
 
 # my $name = Bett->config->{database};
-my $name = "bett061";
+my $name = "bett062";
 
 __PACKAGE__->config(
     schema_class => 'Bett::Schema',
